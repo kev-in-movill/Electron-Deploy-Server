c67af5c25213b42f4be4e95b7f249f9816e55495
kev-in-movill <kevin.you@movill.net>
2019-07-22 18:09:49 +0900

    add

--- server/controllers/package.ts
+++ server/controllers/package.ts
@@ -1,9 +1,13 @@
 import { Request, Response, NextFunction } from "express"
 import { logger } from "../util"
+import path from "path"
 
 export const checkVersion = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.params)
+  console.log(req.params.file)
 
-  logger.log("info", req.body)
-  res.send("OK")
+  const fileName = req.params.file
+  const appPath = path.resolve(__dirname, "..", "..", "release", fileName)
+
+  logger.log("info", appPath)
+  res.download(appPath)
 }
