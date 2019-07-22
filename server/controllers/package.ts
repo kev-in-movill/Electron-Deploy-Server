b5c013aa3ff996cee6f86113adc5d307aa24d9bf
kev-in-movill <kevin.you@movill.net>
2019-07-22 18:39:11 +0900

    add

--- server/controllers/package.ts
+++ server/controllers/package.ts
@@ -5,7 +5,8 @@ import path from "path"
 export const checkVersion = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.params.file)
 
-  const fileName = req.params.file
+  let fileName = req.params.file
+  fileName = fileName.replace(/-/gi, " ")
   const appPath = path.resolve(__dirname, "..", "..", "release", fileName)
 
   logger.log("info", appPath)
